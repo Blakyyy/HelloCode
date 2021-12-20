94159fb9a08fcab66f1953edd3ad54bf7703a445
Daniil <daniil.kobzar299@gmail.com>
2021-12-20 19:48:40 +0100

    // Даны два числа. Показать большее и меньшее число

---
+++ Task0002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int number1 = new Random().Next(1, 101);
+int number2 = new Random().Next(1, 101);
+if(number1 > number2)
+{
+    Console.WriteLine("Первое число - " + number1 + " больше второго - " + number2);
+}
+else if(number1 == number2) 
+{
+    Console.WriteLine("Первое число - " + number1 + " равно вторуму числу - " + number2);
+}
+else
+{
+    Console.WriteLine("Первое число - " + number1 + " Меньше второго числа - " + number2);
+}
