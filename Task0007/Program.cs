3dbb12326dee4e2522df64ff589c9a8baf018364
Daniil <daniil.kobzar299@gmail.com>
2021-12-23 21:58:26 +0100

    Progrgam

---
+++ Task0007/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать числа от -N до N
+Console.WriteLine("Введите отрицацельное число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Выводим числа от отрицательного до положительного");
+for (int i = number; i<=Math.Abs(number); i++)
+{
+    Console.WriteLine(i);
+}
