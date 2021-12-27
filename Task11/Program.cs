5ddff7788478f1f081f8a54c156abca0e221c4a3
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 19:32:54 +0100

    Task 11 working

---
+++ Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10, 100);
+int firstnumber = number%10;
+int secondnumber = (number%100)/10;
+if (firstnumber > secondnumber)
+{
+    Console.WriteLine(number + " Цифра - " + firstnumber);
+}
+else
+{
+    Console.WriteLine(number + " Цифра - " + secondnumber);
+}
