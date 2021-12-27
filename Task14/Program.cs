fdf9499a724f896839e9a3d4f1146a99d9c0f662
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 22:02:59 +0100

    Task 14 working

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int number = new Random().Next(1, 1000);
+int thirddigit = number/100;
+if (thirddigit < 1)
+{
+    Console.WriteLine("Your number " + number + " doesnt have three digits");
+}
+else
+{
+    Console.WriteLine("Your number is " + number + " does have third digit - " + thirddigit);
+}
