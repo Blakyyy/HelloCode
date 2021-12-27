c80f14022a3717ec1b8ff225f962fd2d29d7a52b
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 18:50:53 +0100

    task 5 added

---
+++ Task0005/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Написать программу вычисления значения функции y = f(a)
+int f(int arg1, int arg2)  //Имя функции "f" - (тип данных + агрументы)
+ {   int max = arg1;         // Расписывание функциональности функции
+    if (arg2 > max)
+    {
+        max = arg2;
+    }
+    return max;
+ }
+int result = f(new Random().Next(1, 11), new Random().Next(1, 11));  // Использование функции и добавление переменной
+Console.WriteLine(result);
