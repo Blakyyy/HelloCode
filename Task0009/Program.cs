32d49da84915b2fccbb2058932614a5bf452c6ae
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 19:06:40 +0100

    Task 9 working

---
+++ Task0009/Program.cs
@@ -0,0 +1,4 @@
+﻿//Показать последнюю цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int lastdigit = number%10;
+Console.WriteLine(number + " Последняя цифра - " + lastdigit);
