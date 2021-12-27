ea7231a1bc7a6c6f5e275f5bc3fbedf38257a8bf
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 19:01:14 +0100

    Task 8 working

--- Task0008/Program.cs
+++ Task0008/Program.cs
@@ -1,10 +1,14 @@
 ﻿//Показать четные числа от 1 до N
-int number1 = 1;
-int number2 = 20;
-while (number1 <= number2)
+int n1 = 1;
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (n1 <= N)
 {
-    if(number1%2 == 0)
+    if(n1%2 == 0)
     {
-        Console.WriteLine(number1); number1 = number1 + 1;
+        Console.WriteLine(n1);
+        n1++;
     }
+    else
+        n1++;
 }
