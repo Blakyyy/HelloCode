6e4e30ea6856c10a26e1e8d09b60bf033e9e35a9
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 19:20:40 +0100

    Task 10 works

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int seconddigit = (number%100)/10;
+Console.WriteLine(number + " Вторая цифра - " + seconddigit);
+  
+
