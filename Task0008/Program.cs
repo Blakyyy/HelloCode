5cb3cee872bf4b16103fff9fd0627575fb377ebd
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 18:46:54 +0100

    Task 1, 2,3,4,5,6,7 maded. Task 8 gives error

---
+++ Task0008/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать четные числа от 1 до N
+int number1 = 1;
+int number2 = 20;
+while (number1 <= number2)
+{
+    if(number1%2 == 0)
+    {
+        Console.WriteLine(number1); number1 = number1 + 1;
+    }
+}
