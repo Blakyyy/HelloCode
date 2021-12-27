cdd4aa5f23fced69db899b1d0c328b808f0a973e
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 18:51:13 +0100

    task 6 added

---
+++ Task0006/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Выяснить является ли число чётным
+int number = new Random().Next(1, 11);
+int ostatok = number % 2; // При деление числа на 2 берется его остаток с помощью %
+if (ostatok == 0)
+    {
+        Console.WriteLine("Число четное - " + number);
+    }
+else
+{
+    Console.WriteLine("Число не четное - " + number);
+}
