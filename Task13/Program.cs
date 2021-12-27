3f50808dffa42f3d6f91d53a364a54e4fa558627
Daniil <daniil.kobzar299@gmail.com>
2021-12-27 21:51:20 +0100

    Task 13 working

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿int number1 = new Random().Next(10, 100);
+int number2 = new Random().Next(2, 11);
+int ostatok = number1%number2;
+if (ostatok == 0)
+{
+    Console.WriteLine("Число первое " + number1 + " кратно второму числу - " + number2);
+}
+else
+{
+    Console.WriteLine("Первое число " + number1 + " не кратно второму " + number2 + " остаток - " + ostatok);
+}
