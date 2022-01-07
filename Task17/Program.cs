b103abe4064dacd29fe70d964f0dd64b1fc26607
Daniil <daniil.kobzar299@gmail.com>
2022-01-07 13:18:47 +0100

    Task 17 working

---
+++ Task17/Program.cs
@@ -0,0 +1,12 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+int number = new Random().Next(1, 101);
+int number2 = new Random().Next(1, 11);
+int numbernumber = number2 * number2;
+if(numbernumber == number)
+{
+    Console.WriteLine("Число - " + number + " является увадратом числа - " + number2 );
+}
+else
+{
+    Console.WriteLine("Число - " + number + " не является увадратом числа - " + number2);
+}
