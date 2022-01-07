a32a086a0f76086d552059dfcf1b401a50cad0ee
Daniil <daniil.kobzar299@gmail.com>
2022-01-07 13:00:49 +0100

    Task 16 working

---
+++ Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int number = new Random().Next(1, 8);
+if(number > 5)
+{
+    Console.WriteLine(number + " - Сеголня выходной");
+}
+else
+{
+    Console.WriteLine(number + " - Сегодня рабочий день");
+}
