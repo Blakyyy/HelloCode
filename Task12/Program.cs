58a227cd8766792588764d03a88588b97dfa0f4d
Daniil <daniil.kobzar299@gmail.com>
2022-01-07 11:47:48 +0100

    Доделан Task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,6 @@
+﻿//Удалить вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int thirddigit = number/100;
+int seconddigit = (number%100)/10;
+int firstdigit = (number%100)%10;
+Console.WriteLine(number + " Второй - " + thirddigit + firstdigit);
