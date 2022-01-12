bb2a3f1867beee2e0eb9ce7742c6b7299e7d0dd5
Daniil <daniil.kobzar299@gmail.com>
2022-01-12 15:04:07 +0100

    исправлены ортографические ошибки в Task 17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -4,9 +4,9 @@ int number2 = new Random().Next(1, 11);
 int numbernumber = number2 * number2;
 if(numbernumber == number)
 {
-    Console.WriteLine("Число - " + number + " является увадратом числа - " + number2 );
+    Console.WriteLine("Число - " + number + " является квадратом числа - " + number2 );
 }
 else
 {
-    Console.WriteLine("Число - " + number + " не является увадратом числа - " + number2);
+    Console.WriteLine("Число - " + number + " не является квадратом числа - " + number2);
 }
