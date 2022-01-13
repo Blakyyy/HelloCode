97e998817f28df51fb76290669c14365398cf299
Daniil <daniil.kobzar299@gmail.com>
2022-01-13 18:50:18 +0100

    task 24 added

---
+++ Task24/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+int n = new Random().Next(1, 11);
+int i = 1;
+while (i < n)
+{
+    int squarenumbers = i * i * i;
+    Console.WriteLine("Куб числа " + i + " = " + squarenumbers);
+    i++;
+}
