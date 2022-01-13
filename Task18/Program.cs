462cd7be6053b5c471f0c097560d424e6b2594d4
Daniil <daniil.kobzar299@gmail.com>
2022-01-13 15:40:27 +0100

    Task 18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool check(bool x, bool y)
+{
+    if(!(x || y ) == (!x && !y))
+        return true;
+    else 
+        return false;
+}
+if (check(false, false) && check(false, true) && check(true, false) && check(true, true))
+{
+    Console.WriteLine("Верно");
+}
+else
+{
+    Console.WriteLine("Неверно");
+}
