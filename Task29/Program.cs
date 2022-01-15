2d8c3ab8f466a8ab0f2f97eea3a2d221d15d0382
Daniil <daniil.kobzar299@gmail.com>
2022-01-15 17:34:28 +0100

    Task 29 added

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Вебирите число для N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int result = 0;
+for (i = 1; i <= N; i++)
+{
+    result += i;
+}
+Console.WriteLine(result);
