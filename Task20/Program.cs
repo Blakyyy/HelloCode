a027c267cda7185b7ecc1c8af7ceb2ae1a68578e
Daniil <daniil.kobzar299@gmail.com>
2022-01-15 17:38:26 +0100

    Task 20 added

---
+++ Task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Выберите номер четверти - (1, 2, 3, 4)?");
+int partnumber = Convert.ToInt32(Console.ReadLine());
+if (partnumber == 1)
+{
+    Console.WriteLine("Диапазон возможных координат - (+Infinity, +Infinity");
+}
+else if (partnumber == 2)
+{
+    Console.WriteLine("Диапазон возможных координат - (-Infinity, +Infinity)");
+}
+else if (partnumber == 3)
+{
+    Console.WriteLine("Диапазон возможных координат - (-Infinity, -Infinity)");
+}
+else
+{
+    Console.WriteLine("Диапазон возможных координат - (+Infinity, -Infinity)");
+}
