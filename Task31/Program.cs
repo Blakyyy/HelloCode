d3f4ccb344664c957c85cd589420c96066603278
Daniil <daniil.kobzar299@gmail.com>
2022-01-17 18:21:43 +0100

    Task 31 changed

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задать массив из 8 элементов и вывести их на экран 
 int[] array = new int[8];
 int i = 0;
-for (i = 0; i <= array.Length; i++)
+for (i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(1, 9);
     Console.WriteLine(array[i]);
