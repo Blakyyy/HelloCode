d3a56cd518ec07ac7be508a570cbd9935744162a
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 15:47:33 +0100

    Task 39 added

---
+++ Task39/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int[] array = {2, 4, 1, 7, 4, 5, 7, 6};
+for (int i = 0; i < array.Length; i++)
+{
+    int multiple = array[i] * array[array.Length - i - 1];
+    Console.WriteLine($" Multiplication = {multiple}");
+    if (i >= array.Length / 2 - 1) break;
+}
