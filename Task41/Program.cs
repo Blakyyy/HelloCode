9497040a5fe59a50e3c7811468a6a9c1bebbc39a
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 16:18:46 +0100

    Task 41 added

---
+++ Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+if (a+b>c && a+c>b && c+b>a)
+{
+    System.Console.WriteLine($"A = {a} , B = {b}, C = {c}, All 3 numbers form a triangle");
+}
+else
+{
+    System.Console.WriteLine($"A = {a} , B = {b}, C = {c}, All 3 numbers DONT form a triangle");
+}
