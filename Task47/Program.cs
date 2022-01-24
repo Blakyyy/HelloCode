2d2cc3b23e41714b623179cbc46283f472013c4a
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 20:04:25 +0100

    Task 47 added

---
+++ Task47/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу копирования массива
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i]);
+    Console.Write(" ");
+}
+Console.WriteLine(" ");
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+void PrintArray(int[] newArray)
+{
+    for(int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write(newArray[i]);
+        Console.Write(" ");
+    }
+}
+int[] newArray = CopyArray(array);
+PrintArray(newArray);
