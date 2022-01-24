24f9cea5569a5991b5af3e4ca53d5a669d0d4c2e
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 14:09:12 +0100

    Task 38 added

---
+++ Task38/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] array = new int[20];
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.WriteLine(array[i]);
+    if(i % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine($"Ссума всех чисел стоящих на нечетной позиции = {sum}");
