91cec38f9d52f72b140ebd33379cd819dc471ae8
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 16:29:57 +0100

    Task 42 added

---
+++ Task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+int i = 0;
+int count = 0;
+while (i < 5)
+{
+    Console.WriteLine("Please insert a nummber: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if(a > 0)
+    {
+        count = count + 1; 
+    }
+    i++;
+}
+Console.WriteLine($"The amount of numbers typed using keyboard higher than 0 equals to = {count}");
