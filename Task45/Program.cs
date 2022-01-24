2f6f091a845384aec4218d148aaca9018de55364
Daniil <daniil.kobzar299@gmail.com>
2022-01-24 19:18:56 +0100

    Task 45 added

---
+++ Task45/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать числа Фибоначчи
+int n = 1;
+int n2 = 1;
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n);
+Console.WriteLine(n2);
+while (n2 < num )
+{
+    int fib = n + n2;
+    n = n2;
+    n2 = fib;
+    if (n2 >= num) break;
+    Console.WriteLine(fib);
+}
