203b04101025b63e884fa2141b55aa30f9d73bda
Daniil <daniil.kobzar299@gmail.com>
2022-01-27 19:57:28 +0100

    Task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,33 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+int n = 3;
+int k = 3;
+int[,] array = new int[n, k];
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < k; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine(" ");
+}
+Console.WriteLine(" ");
+
+void ChangeNum(int[,] array)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < k; j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+ChangeNum(array);
