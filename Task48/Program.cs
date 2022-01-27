6521e7594ecaeb45296fddf4874ded19cb91ee87
Daniil <daniil.kobzar299@gmail.com>
2022-01-27 19:25:24 +0100

    Task 48 added

---
+++ Task48/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать двумерный массив размером m×n заполненный целыми числами
+int n = 3;
+int k = 3;
+int[,] array = new int[n, k];
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < k; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j]);
+        Console.Write(" ");
+    }
+    Console.WriteLine(" ");
+}
