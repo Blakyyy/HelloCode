2c30edeea9f201f9c60b5a14ea92203e8ad4a99d
Daniil <daniil.kobzar299@gmail.com>
2022-01-31 19:29:28 +0100

    Task 67 added

---
+++ Task67/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void PrintArray(int n)
+{
+    Console.Write($"{n} ");
+    if (n != 1)
+        PrintArray(n - 1);
+}
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintArray(N);
